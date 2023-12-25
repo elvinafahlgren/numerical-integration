24f02fcfd732f0ab1c60007f9d114bac17a9d4d4
elvinafahlgren <elvina.fahlgren@live.com>
2023-12-25 12:03:17 +0100

    add webapp scaling result graph

--- locustfile.py
+++ locustfile.py
@@ -4,4 +4,4 @@ from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     @task
     def integration(self):
-        self.client.get("http://127.0.0.1:5000/integration")
+        self.client.get("https://webapp-scaling.azurewebsites.net/integration")
